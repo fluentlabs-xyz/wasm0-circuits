b4e6ec0a366096d6ae90da57521046e2928356bf
Stas Stepanov <stanislav@ankr.com>
2023-02-22 18:48:16 +0600

    fix gas_injector support

--- geth-utils/build.rs
+++ geth-utils/build.rs
@@ -2,8 +2,9 @@ use std::env;
 use std::io::{self, Write};
 
 fn main() {
-    let lib_name = "go-geth-utils";
+    let lib_name = "geth-utils";
     let out_dir = env::var("OUT_DIR").unwrap();
+    let manifest_dir = env::var("CARGO_MANIFEST_DIR").unwrap();
 
     // Build
     if let Err(e) = gobuild::Build::new()
@@ -37,6 +38,15 @@ fn main() {
     // Link
     println!("cargo:rustc-link-search=native={}", out_dir);
     println!("cargo:rustc-link-lib=static={}", lib_name);
+
+    let local_libs_subdirs = vec!["linux-amd64", "darwin-amd64", "darwin-aarch64"];
+    for subdir in local_libs_subdirs {
+        let local_libs_path = manifest_dir.clone() + "/../libs/" + subdir;
+        println!("cargo:rustc-link-search={}", &local_libs_path);
+        println!("cargo:rustc-link-arg=-Wl,-rpath,{}", &local_libs_path);
+    }
+    println!("cargo:rustc-link-lib=gas_injector");
+    // println!("cargo:rustc-flags={}", "-l gas_injector -L /usr/local/lib");
 }
 
 fn fail(message: String) {
