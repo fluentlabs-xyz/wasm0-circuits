e60c7b5406af400665b6037db9dd656a22bc381e
Eveniizh <e.zhukov@ankr.com>
2023-07-06 19:57:40 +0700

    Wasm conversion, WIP implement using range
    Implementing `I64ExtendI32` ops
    Using range check to make sure that `value` is i32 data inside
    Solving problem with negative values

--- zkevm-circuits/src/evm_circuit/wasm/wasm_conversion.rs
+++ zkevm-circuits/src/evm_circuit/wasm/wasm_conversion.rs
@@ -27,16 +27,8 @@ pub(crate) struct WasmConversionGadget<F> {
 
     value: Cell<F>,
     value_limbs: [Cell<F>; 8],
-    //value_type: Cell<F>,
     res: Cell<F>,
-    //res_type: Cell<F>,
-
-/*
-    flag_bit: Cell<F>,
-    flag_u8_rem: Cell<F>,
-    flag_u8_rem_diff: Cell<F>,
-*/
-
+    is_value_pos: Cell<F>,
     is_i32_wrap_i64: Cell<F>,
     is_i64_extend_i32_u: Cell<F>,
     is_i64_extend_i32_s: Cell<F>,
@@ -58,18 +50,9 @@ impl<F: Field> ExecutionGadget<F> for WasmConversionGadget<F> {
         let value_limbs = [cb.alloc_u64(), cb.alloc_u64(), cb.alloc_u64(), cb.alloc_u64(),
                            cb.alloc_u64(), cb.alloc_u64(), cb.alloc_u64(), cb.alloc_u64()];
 
-        //let value_type = cb.alloc_common_range_value();
-
         let res = cb.alloc_u64();
-        //let res_type = cb.alloc_common_range_value();
- 
 
-/*
-        let flag_bit = cb.alloc_bit_value();
-        let flag_u8_rem = cb.alloc_common_range_value();
-        let flag_u8_rem_diff = cb.alloc_common_range_value();
-*/
-
+        let is_value_pos = cb.alloc_bit_value();
         let is_i32_wrap_i64 = cb.alloc_bit_value();
         let is_i64_extend_i32_u = cb.alloc_bit_value();
         let is_i64_extend_i32_s = cb.alloc_bit_value();
@@ -83,7 +66,6 @@ impl<F: Field> ExecutionGadget<F> for WasmConversionGadget<F> {
                 values: [value_limbs[i*2].expr(), value_limbs[i*2+1].expr(), 0.expr()],
             });
         }
-           
 
         cb.require_zeros("op_conversion: pick one", vec![
               is_i32_wrap_i64.expr()
@@ -114,42 +96,25 @@ impl<F: Field> ExecutionGadget<F> for WasmConversionGadget<F> {
             }],
         );
 
-
-/*
-        cb.require_zeros("op_conversion: type matches op", vec![
-            is_i32_wrap_i64.expr() * (value_type.expr() - types::I64.expr()),
-            is_i32_wrap_i64.expr() * (res_type.expr() - types::I32.expr()),
-            (is_i64_extend_i32_s.expr() + is_i64_extend_i32_u.expr())
-                * (value_type.expr() - types::I32.expr()),
-            (is_i64_extend_i32_s.expr() + is_i64_extend_i32_u.expr())
-                * (res_type.expr() - types::I64.expr()),
-        ]);
-
-        // TODO: enable this constraint, maybe some range proofs and byte limbs is useful for this.
-        // constraint_builder.push("i32_wrap_i64", Box::new(move |meta| {
-        //         let mut acc = constant_from!(0);
-        //        for i in 0..4 { acc = acc + value.u8_expr(meta, i) * constant_from!(1 << (i * 8)); }
-        //        vec![is_i32_wrap_i64.expr(meta) * (acc - res.expr(meta))]
-        // }),);
-
-        // TODO: enable this constraint, maybe some range proofs and byte limbs is useful for this.
-        // constraint_builder.push("extend op flag bit", Box::new(move |meta| {
-        //        let flag_u8 = value.u8_expr(meta, 3);
-        //        vec![(is_i64_extend_i32_s.expr(meta) + is_i64_extend_i32_u.expr(meta))
-        //                * (flag_bit.expr(meta) * constant_from!(128) + flag_u8_rem.expr(meta) - flag_u8),
-        //             (is_i64_extend_i32_s.expr(meta) + is_i64_extend_i32_u.expr(meta))
-        //                * (flag_u8_rem.expr(meta) + flag_u8_rem_diff.expr(meta) - constant_from!(127)),
-        // ]}),);
-
-        cb.require_zeros("op_conversion: i64_extend_i32_u", vec![
-            is_i64_extend_i32_u.expr() * (res.expr() - value.expr())
-        ]);
-
-        cb.require_zeros("op_conversion: i64_extend_i32_s", {
-            let pad = flag_bit.expr() * ((u32::MAX as u64) << 32).expr();
-            vec![is_i64_extend_i32_s.expr() * (pad + value.expr() - res.expr())]
-        });
-*/
+        cb.require_zeros(
+            "op_conversion: result case of i64_extend_i32",
+            {
+                // Now we are working with i32 that can be signed or not, in both cases only first four limbs is used.
+                // So limbs goes after this must be zero, this check is used to make sure about it.
+                let mut check = value_limbs[4].expr();
+                for i in 5..8 {
+                    check = check + value_limbs[i].expr();
+                }
+                let cond = || is_i64_extend_i32_u.expr() + is_i64_extend_i32_s.expr();
+                let pos_cond = || is_i64_extend_i32_u.expr() + is_i64_extend_i32_s.expr() * is_value_pos.expr();
+                let neg_cond = || is_i64_extend_i32_s.expr() * (1.expr() - is_value_pos.expr());
+                vec![
+                    check * cond(),
+                    ( value.expr() - res.expr() ) * pos_cond(),
+                    ( value.expr() + 0xffffffff_00000000_u64.expr() - res.expr() ) * neg_cond(),
+                ]
+            },
+        );
 
         let opcode = cb.query_cell();
 
@@ -167,14 +132,8 @@ impl<F: Field> ExecutionGadget<F> for WasmConversionGadget<F> {
             same_context,
             value,
             value_limbs,
-            //value_type,
             res,
-            //res_type,
-/*
-            flag_bit,
-            flag_u8_rem,
-            flag_u8_rem_diff,
-*/
+            is_value_pos,
             is_i32_wrap_i64,
             is_i64_extend_i32_u,
             is_i64_extend_i32_s,
@@ -197,42 +156,25 @@ impl<F: Field> ExecutionGadget<F> for WasmConversionGadget<F> {
         let [value, res] = [step.rw_indices[0], step.rw_indices[1]]
             .map(|idx| block.rws[idx].stack_value());
 
+        self.value.assign(region, offset, Value::known(value.to_scalar().unwrap()))?;
+        self.res.assign(region, offset, Value::known(res.to_scalar().unwrap()))?;
+
+        for idx in 0..8 {
+            let limb = (value.0[0] >> (idx * 8)) & 0xff;
+            self.value_limbs[idx].assign(region, offset, Value::<F>::known(F::from(limb)))?;
+        }
+
         match opcode {
             OpcodeId::I32WrapI64 => {
-                self.value.assign(region, offset, Value::known(value.to_scalar().unwrap()))?;
-                //self.value_type.assign(region, offset, Value::known(types::I64.to_scalar().unwrap()))?;
-                self.res.assign(region, offset, Value::known(res.to_scalar().unwrap()))?;
-                //self.res_type.assign(region, offset, Value::known(types::I32.to_scalar().unwrap()))?;
-
                 self.is_i32_wrap_i64.assign(region, offset, Value::known(true.to_scalar().unwrap()))?;
-                for idx in 0..8 {
-                    let limb = (value.0[0] >> (idx * 8)) & 0xff;
-                    self.value_limbs[idx].assign(region, offset, Value::<F>::known(F::from(limb)))?;
-                }
             }
-            OpcodeId::I64ExtendI32 => {
-                let is_psign = true;
-                if is_psign {
-                    self.is_i64_extend_i32_u.assign(region, offset, Value::known(true.to_scalar().unwrap()))?;
-                } else {
-                    self.is_i64_extend_i32_s.assign(region, offset, Value::known(true.to_scalar().unwrap()))?;
-                }
-
-                /*
-                let flag_u8 = value.0[0] as u32 >> (32 - 8);
-                let flag_bit = flag_u8 >> 7;
-                let flag_u8_rem = flag_u8 & 0x7f;
-                let flag_u8_rem_diff = 0x7f - flag_u8_rem;
-
-                self.flag_bit.assign(region, offset, Value::known((flag_bit == 1).to_scalar().unwrap()))?;
-                self.flag_u8_rem.assign(region, offset, Value::known((flag_u8_rem as u64).to_scalar().unwrap()))?;
-                self.flag_u8_rem_diff.assign(region, offset, Value::known((flag_u8_rem_diff as u64).to_scalar().unwrap()))?;
-                */
-
-                self.value.assign(region, offset, Value::known(value.to_scalar().unwrap()))?;
-                //self.value_type.assign(region, offset, Value::known(types::I32.to_scalar().unwrap()))?;
-                self.res.assign(region, offset, Value::known(res.to_scalar().unwrap()))?;
-                //self.res_type.assign(region, offset, Value::known(types::I64.to_scalar().unwrap()))?;
+            OpcodeId::I64ExtendUI32 => {
+                self.is_i64_extend_i32_u.assign(region, offset, Value::known(true.to_scalar().unwrap()))?;
+            }
+            OpcodeId::I64ExtendSI32 => {
+                let is_value_pos = (value.as_u32() <= i32::MAX as u32) as u64;
+                self.is_value_pos.assign(region, offset, Value::<F>::known(F::from(is_value_pos)))?;
+                self.is_i64_extend_i32_s.assign(region, offset, Value::known(true.to_scalar().unwrap()))?;
             }
             _ => unreachable!("not supported opcode: {:?}", opcode),
         };
@@ -268,4 +210,36 @@ mod test {
             Drop
         });
     }
+
+    #[test]
+    fn test_i64_extend_u_i32() {
+        run_test(bytecode! {
+            I32Const[0]
+            I64ExtendUI32
+            Drop
+            I32Const[0xffffffff]
+            I64ExtendUI32
+            Drop
+            I32Const[0x0f0f0f0f]
+            I64ExtendUI32
+            Drop
+        });
+    }
+
+    #[test]
+    fn test_i64_extend_s_i32() {
+        run_test(bytecode! {
+            I32Const[0]
+            I64ExtendSI32
+            Drop
+            I32Const[0x70ffffff]
+            I64ExtendSI32
+            Drop
+            I32Const[-0x70ffffff]
+            I64ExtendSI32
+            Drop
+        });
+    }
+
+
 }
